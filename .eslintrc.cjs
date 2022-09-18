7c4a53c9727292a7038a88dd45d8d0aa4524d36f
pb <priyabrata.priyadarshi@incture.com>
2022-09-18 13:50:52 +0530

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', 'simple-import-sort'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'react/jsx-key': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Hi from pb...</div>;
+};
+
+export default App;
